4d145a1703adaa2e47d402f6fe69817299cc6ad7
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-14 17:26:39 -0400

    fixed signup flow

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
 
 	class Meta(UserCreationForm.Meta):
 		model = CustomUser
-		fields = ('email', 'username', 'first_name', 'last_name',)
+		fields = ('first_name', 'last_name','password1', 'password2',)
 	field_order = ['username', 'email','first_name', 'last_name','password1', 'password2',]
 
 
