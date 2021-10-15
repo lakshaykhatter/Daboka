9dc353af43b5f589f74882a163410cf824e69b03
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-14 20:52:31 -0400

    made firstname and lastname required fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
+	first_name = forms.CharField()
+	last_name = forms.CharField()
+	
 	class Meta(UserCreationForm.Meta):
 		model = CustomUser
+
 		fields = ('username','email','first_name', 'last_name','password1', 'password2',)
 	field_order = ['username', 'email','first_name', 'last_name','password1', 'password2',]
 	
@@ -17,12 +20,3 @@ class CustomUserChangeForm(UserChangeForm):
 	class Meta:
 		model = CustomUser
 		fields = ('email', 'username', 'first_name', "last_name")
-
-# class SignupForm(forms.Form):
-# 	first_name = forms.CharField(max_length=30, label='First name')
-# 	last_name = forms.CharField(max_length=30, label='last name')
-
-# 	def signup(self, request, user):
-# 		user.first_name = self.cleaned_data['first_name']
-# 		user.last_name = self.cleaned_data['last_name']
-# 		user.save()
