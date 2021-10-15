337a2eee979da0b6bfa45f161a90b0c8af047578
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-14 20:47:57 -0400

    fixed signup form bug

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,12 +6,23 @@ class CustomUserCreationForm(UserCreationForm):
 
 	class Meta(UserCreationForm.Meta):
 		model = CustomUser
-		fields = ('first_name', 'last_name','password1', 'password2',)
+		fields = ('username','email','first_name', 'last_name','password1', 'password2',)
 	field_order = ['username', 'email','first_name', 'last_name','password1', 'password2',]
-
+	
+	def save(self, commit=True):
+		return None
 
 class CustomUserChangeForm(UserChangeForm):
 
 	class Meta:
 		model = CustomUser
 		fields = ('email', 'username', 'first_name', "last_name")
+
+# class SignupForm(forms.Form):
+# 	first_name = forms.CharField(max_length=30, label='First name')
+# 	last_name = forms.CharField(max_length=30, label='last name')
+
+# 	def signup(self, request, user):
+# 		user.first_name = self.cleaned_data['first_name']
+# 		user.last_name = self.cleaned_data['last_name']
+# 		user.save()
