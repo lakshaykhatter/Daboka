ab53b34b840fd5dddc7921d426a9e777ad51517b
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-26 11:15:40 -0400

    created staging environmnet settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,12 +1,21 @@
 from config.settings.base import *
 
+
+DATABASES = {
+	"default": env.dj_db_url("DATABASE_URL")
+}
+
+
+
+DEBUG = env.bool("DJANGO_DEBUG")
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
-DEFAULT_FROM_EMAIL = "no_replay@daboka.com"
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = 'sendgrid_password'
-EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = env("DJANGO_DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env("DJANGO_EMAIL_HOST")
+EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("DJANGO_EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
