caa929acc86e7f904cb3fc2fd8bee98b03ef6f33
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-29 15:20:09 -0400

    fixed dashboard url

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from accounts.views import LoginView	
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admindasbhoard/', admin.site.urls),
     path('accounts/login/', LoginView.as_view(), name="account_login"),
     path('posts/', include("posts.urls")),
     path('accounts/', include('allauth.urls')),
