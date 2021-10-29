869716d42cfbe9c2cf3f2077f9b53ed937271640
Lakshay Khatter <lakshaykhatter@gmail.com>
2021-10-29 15:50:04 -0400

    fixed allowed hosts for production

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,7 +20,7 @@ DEBUG = True
 
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1",".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1",".herokuapp.com", "www.daboka.com". "daboka.com"]
 
 # APPS
 # ------------------------------------------------------------------------------
